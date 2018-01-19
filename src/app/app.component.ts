94bc7b595597a693b8ad899ce49b0fec43b1dc6d
Carlos Tenorio <ctenorio@bbsoftbalear.com>
2018-01-19 09:54:59 +0100

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+﻿import { Component, NgModule, VERSION } from '@angular/core'
+import { BrowserModule, DomSanitizer, SafeUrl } from '@angular/platform-browser'
+
+import { AppService } from 'app/app.service';
+
+import * as blobUtil from 'blob-util';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +11,60 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  name: string;
+  picture: SafeUrl;
+  namePerson: string;
+  person1: any;
+  person2: any;
+
+  constructor(
+    private sanitizer: DomSanitizer,
+    public appService: AppService
+  ) {
+    this.name = `Angular! v${VERSION.full}`;
+    this.initTable();
+    this.setImage(0);
+  }
+
+  initTable() {
+    this.appService.persons.add({
+      name: 'Nicolas',
+      parts: []
+    });
+  }
+
+  saveImage(personName: string, inputId: string) {
+    var file1 = (<HTMLInputElement>document.getElementById(inputId)).files[0];
+    var fileReader = new FileReader();
+    var self = this;
+    fileReader.onloadend = function(e) {
+      var arrayBuffer = fileReader.result;
+      var fileType = (<HTMLInputElement>document.getElementById('file-type')).value;
+      blobUtil.arrayBufferToBlob(arrayBuffer, fileType)
+        .then(function(blob) {
+          self.appService.persons.where('name').equals('Nicolas')
+            .modify(person => {
+              person.parts.push({
+                image: blob
+              });
+            });
+        }).catch(
+        console.log.bind(console)
+        );
+    };
+    fileReader.readAsArrayBuffer(file1);
+  }
+
+  setImage(idx: number) {
+    this.appService.persons.where('name').equals('Nicolas').toArray()
+      .then((person) => {
+        this.namePerson = person[0].name;
+        this.picture = this.sanitizer.bypassSecurityTrustUrl(URL.createObjectURL(person[0].parts[idx].image));
+      });
+  }
+
+  deleteDB() {
+    this.appService.persons.clear();
+  }
+
 }
---
+++ src/app/app.service.ts
@@ -0,0 +1,24 @@
+﻿import { Injectable } from '@angular/core';
+import Dexie from 'dexie';
+
+@Injectable()
+export class AppService extends Dexie {
+
+  public persons: Dexie.Table<IPerson, number>;
+
+  constructor() {
+    super('IDBBUGEDGE');
+    this.version(1).stores({
+      persons: "name"
+    });
+  }
+}
+
+export interface IPerson {
+  name: string;
+  parts: IPart[];
+}
+
+export interface IPart {
+  image: Blob;
+}
